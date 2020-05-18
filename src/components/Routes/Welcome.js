dd408d48e1f2a73989c3da71114c28c8060a4b97
Divhya Bharathy <57354024+divhya@users.noreply.github.com>
2020-05-18 16:32:26 +0530

    commit 1
    Image rendering poc is done.

---
+++ src/components/Routes/Welcome.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {BrowserRouter ,Switch,Route,withRouter} from 'react-router-dom';
+//import {Route} from 'react-router-dom';
+import MainRegister from '../SignUp/MainRegister';
+import NavigateBar from '../frontpage/navbar';
+import Login from '../SignIn/Login';
+import GetImages from '../CheckImage/image';
+
+
+class Welcome extends Component{
+render(){
+    return(
+        <div>
+           
+        <BrowserRouter>
+        <NavigateBar/>
+            <Switch>
+            <Route exact path="/signUp" component={withRouter(MainRegister)} />
+
+             {/* <Route exact path="/signup" component={Register}/> 
+             <Route exact path="/a" component={Register}/> */}
+             <Route exact path="/signIn" component={withRouter(Login)}/>
+             <Route exact path="/showimage" component={withRouter(GetImages)}/>
+            </Switch>
+       
+        </BrowserRouter>
+        </div>
+        
+        
+    );
+}
+}
+
+export default Welcome
